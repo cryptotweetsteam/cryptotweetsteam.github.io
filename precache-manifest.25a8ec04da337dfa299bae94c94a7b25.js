b75f993ba9c1064eaf69d722870a4f24565ee1a2
Kevin Pan <temp@temp.com>
2021-03-07 17:18:13 -0500

    Updates

---
+++ precache-manifest.25a8ec04da337dfa299bae94c94a7b25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c92c9e3aeb95622133874fd95cbb1774",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13840a145fc8dfad35c0",
+    "url": "/static/css/main.54a0cdb6.chunk.css"
+  },
+  {
+    "revision": "08a43e623ae5d4ef3f29",
+    "url": "/static/js/2.7f11bc1a.chunk.js"
+  },
+  {
+    "revision": "13840a145fc8dfad35c0",
+    "url": "/static/js/main.3549bb80.chunk.js"
+  },
+  {
+    "revision": "8e689cd4d3827e59490f",
+    "url": "/static/js/runtime-main.e9c2ae90.js"
+  }
+]);
